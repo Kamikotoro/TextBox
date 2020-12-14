87d7ce77ca1f8adc347f8a42d1332f6e1e64713b
Исаков Данила Денисович <211808@mck-ktits.ru>
2020-12-14 08:51:28 +0300

    Добавьте файлы проекта.

---
+++ TextBox/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace TextBox
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TextBox tBox = new TextBox("лол", ConsoleColor.Blue);
+            TextBox.TextUpdated = TextChange;
+            while(true)
+            {
+                tBox.Text = Console.ReadLine();
+                tBox.Output();
+                if (tBox.Text == "quit")
+                {
+                    break;
+                }
+                else if (Enum.TryParse(tBox.Text,out ConsoleColor color))
+                {
+                    tBox.FontColor = color;
+                }
+            }
+        }
+        static void TextChange(string oldText, string newText)
+        {
+            Console.WriteLine($"Текст был изменен с {oldText} на {newText}");
+        }
+    }
+}
---
+++ TextBox/TextBox.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TextBox
+{
+    public class TextBox
+    {
+        public delegate void TextChanged(string oldText, string newText);
+        public static TextChanged TextUpdated;
+        private string text;
+        private ConsoleColor Color = ConsoleColor.White;
+        public ConsoleColor FontColor
+        {
+            get => Color;
+            set => Color = value;
+        }
+        public string Text
+        {
+            get => text;
+            set
+            {
+                string oldText = text;
+                text = value;
+                TextUpdated?.Invoke(oldText, value);
+            }
+        }
+
+        public TextBox(string text)
+        {
+            this.text = text;
+        }
+        public TextBox(string text, ConsoleColor fontColor) : this(text)
+        {
+            this.Color = fontColor;
+        }
+       
+        public void Clear()
+        {
+            text = "";
+        }
+        public void Output()
+        {
+            Console.ForegroundColor = Color;
+            Console.BackgroundColor = ConsoleColor.DarkBlue;
+            Console.WriteLine(text);
+            Console.ResetColor();
+        }
+    }
+}
